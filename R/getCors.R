5c10bae4aceebe1ad4c69aee3f5646120c6d0ebe
nosarcasm <raneff@gmail.com>
2018-10-15 12:56:35 -0400

    testing fix

--- R/getCors.R
+++ R/getCors.R
@@ -99,9 +99,7 @@ getCors <- function(inputMat, design, inputMatB = NULL, impute = FALSE, corrType
 				return(list(corrs = corr, pvals = pval, nsamps = nsamp))
 			}
 		if(!identical(cl,FALSE)){
-            adiscretize = arules::discretize
-            adiscretizeDF = arules::discretizeDF
-			parallel::clusterExport(cl=cl,c("matCorr","matNSamp","matCorSig","adiscretize","adiscretizeDF"))
+			parallel::clusterExport(cl=cl,c("matCorr","matNSamp","matCorSig"))
 			##TODO: calculate the correlation in chunks and write out to intermediate file
 			groupMatLists = parallel::parLapply(cl=cl,groupList,calcCorrs,
 			                          corrType=corrType,impute=impute, k=k,k_iter_max=k_iter_max)
@@ -140,9 +138,7 @@ getCors <- function(inputMat, design, inputMatB = NULL, impute = FALSE, corrType
 			return(list(corrs = corr, pvals = pval, nsamps = nsamp))
 		}
 		if(!identical(cl,FALSE)){
-            adiscretize = arules::discretize
-            adiscretizeDF = arules::discretizeDF
-			parallel::clusterExport(cl=cl,c("matCorr","matNSamp","matCorSig","adiscretize","adiscretizeDF"))
+			parallel::clusterExport(cl=cl,c("matCorr","matNSamp","matCorSig"))
 			##replacement for python zip() function to allow submission as parallel job
 			zipLists = list()
 			for (i in 1:length(designRes[[1]])){
--- R/matCorr.R
+++ R/matCorr.R
@@ -16,7 +16,7 @@
 
 matCorr <- function(matA, corrType, use = "pairwise.complete.obs", matB = NULL, secondMat = FALSE,
                     k=5,k_iter_max=10){
-
+    library("arules",lib.loc="~/.RlibDGCA")
 	if(!secondMat){
 		if(corrType %in% "pearson"){
 			corrs = cor(matA,use=use,method="pearson")
@@ -25,7 +25,7 @@ matCorr <- function(matA, corrType, use = "pairwise.complete.obs", matB = NULL, 
 			corrs = cor(matA,use=use,method="spearman")
 		}
 		if(corrType %in% "mutualinformation"){
-			matA_discrete = adiscretizeDF(data.frame(matA), 
+			matA_discrete = arules::discretizeDF(data.frame(matA), 
 			                                     default=list("method"="cluster", 
 			                                                  "centers"=k,"iter.max"=k_iter_max))
 			corrs = infotheo::mutinformation(matA_discrete)
@@ -56,8 +56,8 @@ matCorr <- function(matA, corrType, use = "pairwise.complete.obs", matB = NULL, 
 
 			for(col1 in 1:length(colnames(df1))){
 			  for(col2 in col1:length(colnames(df2))){
-			    x_dis = adiscretize(x=as.numeric(df1[,col1]), method="cluster", centers=k,iter.max=k_iter_max)
-			    y_dis = adiscretize(x=as.numeric(df2[,col2]), method="cluster", centers=k,iter.max=k_iter_max)
+			    x_dis = arules::discretize(x=as.numeric(df1[,col1]), method="cluster", centers=k,iter.max=k_iter_max)
+			    y_dis = arules::discretize(x=as.numeric(df2[,col2]), method="cluster", centers=k,iter.max=k_iter_max)
 			    mi_df_2mat[col1,col2] = infotheo::mutinformation(x_dis,y_dis)
 			    mi_df_2mat[col2,col1] = mi_df_2mat[col1,col2]
 			  }
